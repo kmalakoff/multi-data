9168dc163cebc1a2f2c065882574eabc8ec476f1
Kevin Malakoff <kmalakoff@gmail.com>
2021-10-17 19:43:38 -0700

    Added binary data

---
+++ test/lib/polyfill.cjs
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+function polyfill() {
+  if (typeof Buffer !== "undefined" && !Buffer.from) {
+    Buffer.from = function from(data, encoding) {
+      return new Buffer(data, encoding);
+    };
+  }
+}
+polyfill();
