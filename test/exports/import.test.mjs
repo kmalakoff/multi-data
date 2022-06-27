3af86b93bc168fd135171d2eb2648670db975abf
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-27 00:13:45 -0700

    Update ts-dev-stack

--- test/exports/import.test.mjs
+++ test/exports/import.test.mjs
@@ -1,12 +1,12 @@
-import assert from "assert";
-import MultiData from "multi-data";
+import assert from 'assert';
+import MultiData from 'multi-data';
 
-describe("exports .mjs", function () {
-  it("append with headers", function () {
-    const boundary = "test-boundary";
+describe('exports .mjs', function () {
+  it('append with headers', function () {
+    const boundary = 'test-boundary';
     const form = new MultiData(boundary);
-    const options = { headers: { "some-header": "header-value" } };
-    form.append("section1", "some data", options);
-    assert.ok(~form.toString().indexOf("some-header"));
+    const options = { headers: { 'some-header': 'header-value' } };
+    form.append('section1', 'some data', options);
+    assert.ok(~form.toString().indexOf('some-header'));
   });
 });
--- test/exports/import.test.ts
+++ test/exports/import.test.ts
@@ -1,12 +1,12 @@
-import assert from "assert";
-import MultiData, { Options } from "multi-data";
+import assert from 'assert';
+import MultiData, { Options } from 'multi-data';
 
-describe("exports .ts", function () {
-  it("append with headers", function () {
-    const boundary = "test-boundary";
+describe('exports .ts', function () {
+  it('append with headers', function () {
+    const boundary = 'test-boundary';
     const form = new MultiData(boundary);
-    const options: Options = { headers: { "some-header": "header-value" } };
-    form.append("section1", "some data", options);
-    assert.ok(~form.toString().indexOf("some-header"));
+    const options: Options = { headers: { 'some-header': 'header-value' } };
+    form.append('section1', 'some data', options);
+    assert.ok(~form.toString().indexOf('some-header'));
   });
 });
