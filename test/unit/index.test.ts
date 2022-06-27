6afbbd0578e3f956e8cb9463bd12b2f8cdcab864
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-27 00:13:45 -0700

    Update ts-dev-stack

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,86 +1,62 @@
-import assert from "assert";
-import MultiData from "multi-data";
-import encodeUTF8 from "../lib/encodeUTF8.cjs";
+import assert from 'assert';
+import MultiData from 'multi-data';
+import encodeUTF8 from '../lib/encodeUTF8.cjs';
 
-describe("form-data", function () {
-  describe("constructor", function () {
-    it("Error: constructor expects a boundary", function () {
+describe('form-data', function () {
+  describe('constructor', function () {
+    it('Error: constructor expects a boundary', function () {
       assert.throws(() => new MultiData(undefined));
     });
 
-    it("boundary", function () {
-      const boundary = "test-boundary";
+    it('boundary', function () {
+      const boundary = 'test-boundary';
       const form = new MultiData(boundary);
       assert.equal(form.boundary, boundary);
     });
   });
 
-  describe("toString", function () {
-    it("no append", function () {
-      const boundary = "test-boundary";
+  describe('toString', function () {
+    it('no append', function () {
+      const boundary = 'test-boundary';
       const form = new MultiData(boundary);
       assert.equal(form.toString(), `--${boundary}--`);
     });
   });
 
-  describe("append", function () {
-    it("Error: append expects a name", function () {
-      const boundary = "test-boundary";
+  describe('append', function () {
+    it('Error: append expects a name', function () {
+      const boundary = 'test-boundary';
       const form = new MultiData(boundary);
-      assert.throws(() => form.append(undefined, "some data"));
+      assert.throws(() => form.append(undefined, 'some data'));
     });
 
-    it("Error: append expects data", function () {
-      const boundary = "test-boundary";
+    it('Error: append expects data', function () {
+      const boundary = 'test-boundary';
       const form = new MultiData(boundary);
-      assert.throws(() => form.append("section1", undefined));
+      assert.throws(() => form.append('section1', undefined));
     });
 
-    it("append", function () {
-      const boundary = "test-boundary";
+    it('append', function () {
+      const boundary = 'test-boundary';
       const form = new MultiData(boundary);
-      form.append("section1", "some data");
-      assert.equal(
-        form.toString(),
-        [`--${boundary}`, 'Content-Disposition: form-data; name="section1"', "", "some data", `--${boundary}--`].join(
-          "\r\n"
-        )
-      );
+      form.append('section1', 'some data');
+      assert.equal(form.toString(), [`--${boundary}`, 'Content-Disposition: form-data; name="section1"', '', 'some data', `--${boundary}--`].join('\r\n'));
     });
 
-    it("append binary", function () {
-      const boundary = "test-boundary";
+    it('append binary', function () {
+      const boundary = 'test-boundary';
       const form = new MultiData(boundary);
-      form.append("section1", encodeUTF8("some data"));
-      assert.equal(
-        form.toString(),
-        [
-          `--${boundary}`,
-          'Content-Disposition: form-data; name="section1"',
-          "",
-          encodeUTF8("some data").toString(),
-          `--${boundary}--`,
-        ].join("\r\n")
-      );
+      form.append('section1', encodeUTF8('some data'));
+      assert.equal(form.toString(), [`--${boundary}`, 'Content-Disposition: form-data; name="section1"', '', encodeUTF8('some data').toString(), `--${boundary}--`].join('\r\n'));
     });
 
-    it("append with headers", function () {
-      const boundary = "test-boundary";
+    it('append with headers', function () {
+      const boundary = 'test-boundary';
       const form = new MultiData(boundary);
-      form.append("section1", "some data", {
-        headers: { "some-header": "header-value" },
+      form.append('section1', 'some data', {
+        headers: { 'some-header': 'header-value' },
       });
-      assert.equal(
-        form.toString(),
-        [
-          `--${boundary}`,
-          'Content-Disposition: form-data; name="section1"',
-          "some-header: header-value",
-          "",
-          "some data",
-          `--${boundary}--`,
-        ].join("\r\n")
-      );
+      assert.equal(form.toString(), [`--${boundary}`, 'Content-Disposition: form-data; name="section1"', 'some-header: header-value', '', 'some data', `--${boundary}--`].join('\r\n'));
     });
   });
 });
