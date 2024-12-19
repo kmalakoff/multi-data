01234eee4f33c997dac5d00cc3e4602d1edfad37
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-19 08:17:57 -0800

    Upgrade modules

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-module.exports = void 0;
-function _classCallCheck(instance, Constructor) {
+Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function() {
+        return MultiData;
+    }
+});
+function _class_call_check(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
         throw new TypeError("Cannot call a class as a function");
     }
@@ -11,9 +16,9 @@ function _classCallCheck(instance, Constructor) {
 var MultiData = /*#__PURE__*/ function() {
     "use strict";
     function MultiData(boundary) {
-        _classCallCheck(this, MultiData);
+        _class_call_check(this, MultiData);
         this.lines = [];
-        if (boundary === undefined) throw new TypeError("boundary expected");
+        if (boundary === undefined) throw new TypeError('boundary expected');
         this.boundary = boundary;
     }
     var _proto = MultiData.prototype;
@@ -24,15 +29,15 @@ var MultiData = /*#__PURE__*/ function() {
    * @param data The part data.
    * @param options Pass headers in the options for custom part headers.
    */ _proto.append = function append(name, data, options) {
-        if (name === undefined) throw new TypeError("name expected");
-        if (data === undefined) throw new TypeError("data expected");
+        if (name === undefined) throw new TypeError('name expected');
+        if (data === undefined) throw new TypeError('data expected');
         this.lines.push("--".concat(this.boundary));
         this.lines.push('Content-Disposition: form-data; name="'.concat(name, '"'));
         if (options && options.headers) {
             var headers = options.headers;
             for(var key in headers)this.lines.push("".concat(key, ": ").concat(headers[key]));
         }
-        this.lines.push("");
+        this.lines.push('');
         this.lines.push(data);
         return this;
     };
@@ -40,10 +45,10 @@ var MultiData = /*#__PURE__*/ function() {
    * After appending data, use toString() to concatenate the form data for your request.
    */ _proto.toString = function toString() {
         this.lines.push("--".concat(this.boundary, "--"));
-        var string = this.lines.join("\r\n");
+        var string = this.lines.join('\r\n');
         this.lines.pop();
         return string;
     };
     return MultiData;
 }();
-module.exports = MultiData;
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
