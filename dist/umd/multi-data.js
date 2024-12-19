ff1d4b6862418332ed933ff5abc05d26b33b51a0
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-19 07:29:32 -0800

    Upgrade modules

--- dist/umd/multi-data.js
+++ dist/umd/multi-data.js
@@ -4,16 +4,16 @@
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.multiData = factory());
 })(this, (function () { 'use strict';
 
-  function _classCallCheck(instance, Constructor) {
+  function _class_call_check(instance, Constructor) {
       if (!(instance instanceof Constructor)) {
           throw new TypeError("Cannot call a class as a function");
       }
   }
   var MultiData = /*#__PURE__*/ function() {
       function MultiData(boundary) {
-          _classCallCheck(this, MultiData);
+          _class_call_check(this, MultiData);
           this.lines = [];
-          if (boundary === undefined) throw new TypeError("boundary expected");
+          if (boundary === undefined) throw new TypeError('boundary expected');
           this.boundary = boundary;
       }
       var _proto = MultiData.prototype;
@@ -24,15 +24,15 @@
      * @param data The part data.
      * @param options Pass headers in the options for custom part headers.
      */ _proto.append = function append(name, data, options) {
-          if (name === undefined) throw new TypeError("name expected");
-          if (data === undefined) throw new TypeError("data expected");
+          if (name === undefined) throw new TypeError('name expected');
+          if (data === undefined) throw new TypeError('data expected');
           this.lines.push("--".concat(this.boundary));
           this.lines.push('Content-Disposition: form-data; name="'.concat(name, '"'));
           if (options && options.headers) {
               var headers = options.headers;
               for(var key in headers)this.lines.push("".concat(key, ": ").concat(headers[key]));
           }
-          this.lines.push("");
+          this.lines.push('');
           this.lines.push(data);
           return this;
       };
@@ -40,7 +40,7 @@
      * After appending data, use toString() to concatenate the form data for your request.
      */ _proto.toString = function toString() {
           this.lines.push("--".concat(this.boundary, "--"));
-          var string = this.lines.join("\r\n");
+          var string = this.lines.join('\r\n');
           this.lines.pop();
           return string;
       };
