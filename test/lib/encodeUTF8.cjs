94a942c817a9da7f17ad953e3be468d8567e6a84
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 22:18:18 -0800

    Upgrade modules

--- test/lib/encodeUTF8.cjs
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-require('./polyfill.cjs');
-const hasBuffer = typeof Buffer !== 'undefined';
-
-module.exports = function encodeUTF8(s) {
-  return hasBuffer ? new Uint8Array(Buffer.from(s, 'utf8')) : Uint8Array.from(s, (x) => x.charCodeAt(0));
-};
