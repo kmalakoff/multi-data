295831b3bc8816565c536dc68e22e50b64cd737e
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-17 17:47:56 -0700

    Upgrade modules

--- test/exports/umd.test.cjs
+++ test/exports/umd.test.cjs
@@ -1,10 +1,17 @@
 const assert = require('assert');
-const MultiData = require('multi-data/dist/umd/multi-data.cjs');
 
-describe('exports multi-data/dist/umd/multi-data.cjs', () => {
+let umd = null;
+try {
+  umd = require('multi-data/umd');
+} catch (_) {
+  umd = require('multi-data/dist/umd/multi-data.cjs');
+}
+const multiData = typeof window !== 'undefined' ? window.multiData : umd.default || umd;
+
+describe('exports umd', () => {
   it('append with headers', () => {
     const boundary = 'test-boundary';
-    const form = new MultiData(boundary);
+    const form = new multiData(boundary);
     const options = { headers: { 'some-header': 'header-value' } };
     form.append('section1', 'some data', options);
     assert.ok(~form.toString().indexOf('some-header'));
