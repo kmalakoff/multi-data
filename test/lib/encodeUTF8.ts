c239ceb9d90f81d6059afa45d926e31e6bb14499
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-03 23:45:45 -0800

    Migrate configuration to tsds-config and refactor encodeUTF8

--- test/lib/encodeUTF8.ts
+++ test/lib/encodeUTF8.ts
@@ -1,12 +1,15 @@
+// Feature detection - no global modifications per polyfill-removal plans
 const hasBuffer = typeof Buffer !== 'undefined';
+const hasBufferFrom = hasBuffer && typeof Buffer.from === 'function' && Buffer.from !== Uint8Array.from;
 
-if (hasBuffer && !Buffer.from) {
-  // @ts-expect-error
-  Buffer.from = function from(data, encoding) {
-    return new Buffer(data, encoding);
-  };
+function bufferFrom(data: string, encoding?: BufferEncoding): Buffer {
+  if (hasBufferFrom) {
+    return Buffer.from(data, encoding);
+  }
+  // Node 0.8-4.x compatibility: use Buffer constructor
+  return new Buffer(data, encoding);
 }
 
-export default function encodeUTF8(s) {
-  return hasBuffer ? new Uint8Array(Buffer.from(s, 'utf8')) : Uint8Array.from(s, (x: string) => x.charCodeAt(0));
+export default function encodeUTF8(s: string): Uint8Array {
+  return hasBuffer ? new Uint8Array(bufferFrom(s, 'utf8')) : Uint8Array.from(s, (x: string) => x.charCodeAt(0));
 }
