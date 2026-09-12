0cbc051728de78ff9f1d20da08c55dcbb974981e
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-11 18:23:03 -0700

    Add Node.js compatibility layer and update README examples

--- test/lib/encodeUTF8.ts
+++ test/lib/encodeUTF8.ts
@@ -1,14 +1,6 @@
-// Feature detection - no global modifications per polyfill-removal plans
-const hasBuffer = typeof Buffer !== 'undefined';
-const hasBufferFrom = hasBuffer && typeof Buffer.from === 'function' && Buffer.from !== Uint8Array.from;
+import { bufferFrom } from './compat.ts';
 
-function bufferFrom(data: string, encoding?: BufferEncoding): Buffer {
-  if (hasBufferFrom) {
-    return Buffer.from(data, encoding);
-  }
-  // Node 0.8-4.x compatibility: use Buffer constructor
-  return new Buffer(data, encoding);
-}
+const hasBuffer = typeof Buffer !== 'undefined';
 
 export default function encodeUTF8(s: string): Uint8Array {
   return hasBuffer ? new Uint8Array(bufferFrom(s, 'utf8')) : Uint8Array.from(s, (x: string) => x.charCodeAt(0));
